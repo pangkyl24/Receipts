f8d356443c0a34950d01ae85d89b3b8f0895651d
pangkyl24 <77285931+pangkyl24@users.noreply.github.com>
2021-10-21 19:06:18 -0400

    Update Printer.java

--- Printer.java
+++ Printer.java
@@ -59,3 +59,25 @@ public class Printer{
 }
 
 //Roughly 15 minutes to type in everything
+
+//DESCRIPTION OF PROCESS
+/*
+* Right now, I would consider my project to be complete. At the end of the block, I had already completed the Kiosk Class with only 1-2 syntax or logical errors that I could easily solve
+* I was halfway done with the printer class, but it was mainly just formatting and a few other things I had to finish which didn't require too much time.
+* 
+* While I was doing this, I knew that my code had to have a clear algorithm from the beginning, for the first few minutes of having this assignment, I didnt even write anything out, I just planned ahead for what I was going to write.
+* Eventually I settled on using a lot of the display/UI features in the example code provided and modified it in order to better suit my project 
+* As for the algorithm, I used a do-while loop to ensure that there could only be 9 items bought, and used numPurchases as the counting variable to note down how many items were bought.
+* So far, I have had very little issues. While I work, normally I just type code until it works well
+* This time I decided to think about what direction I want to push my code towards before writing
+* This is very useful since by doing this beforehand, I can minimize any logical errors and only focus on the syntax ones, which are super quick and easy to fix.
+*/
+
+
+
+
+
+
+
+
+
